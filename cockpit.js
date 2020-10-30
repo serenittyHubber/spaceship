ba9921c4015b2ef25c7d9d9c03f6893391bbeeae
serenittyHubber <carlos.avellar@gmail.com>
2020-10-30 09:20:33 -0300

    Update local cockpit.js

--- cockpit.js
+++ cockpit.js
@@ -1,5 +1,7 @@
 // The intent of this files is to  host the Spacechip angine
 
-// THe spaceship must be Stronger than ever
+// THe spaceship must be strongger than ever
 
-// This is an gedget to up parte of the spaceship
+// This is an gadget to up parte of the spaceship
+
+// I warn you that Batman is down here, in case of Coringa come from the remote repository.
